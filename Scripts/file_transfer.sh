114afae5dbb3aa5bff2ba79df24ef947f9078f62
chaochin <earthshaking001@hotmail.com>
2013-04-22 17:49:40 +0800

    Update file_transfer.sh

--- Scripts/file_transfer.sh
+++ Scripts/file_transfer.sh
@@ -6,7 +6,8 @@ if [ "$1" == "" ] || [ "$2" == "" ]; then
         echo "     # File size:2MB, Number of loop:5 #" 
         echo "     ###################################"
 else
-time=$(date)
+echo -e "Loop\tElapsed time(seconds)\tMD5 comparison result\tSize comparison result" | tee -a /root/test.log
+start_time=$(date +%Y%m%d%H%M%S)
 dd if=/dev/zero of=random_file bs=1024k count=$1
 host_md5=$(md5sum random_file)
 host_size=$(du -sh random_file)
@@ -14,86 +15,79 @@ echo "Copy random_file from Host to Guest VM1"
 scp_start=$(date +%s)
 scp random_file 192.168.20.2:~
 scp_end=$(date +%s)
-echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
+#echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
+echo -n -e "\t$(($scp_end-$scp_start))\t" | tee -a /root/test.log
 vm1_md5=$(ssh 192.168.20.2 "md5sum random_file")
 vm1_size=$(ssh 192.168.20.2 "du -sh random_file")
-echo "Copy random_file from Guest VM1 to Guest VM2"
-scp_start=$(date +%s)
-ssh 192.168.20.2 "sshpass -p 'root' scp random_file root@192.168.20.4:~"
-scp_end=$(date +%s)
-echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
-vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum rand
-om_file"")
-vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh ran
-dom_file"")
-echo "Check md5 from Host and Guest VMs:"
-if [ "$host_md5" == "$vm1_md5" ] && [ "$vm1_md5" == "$vm2_md5" ]; then
-        echo "MD5 match"
+echo "Check md5 from Host and Guest VM1:"
+if [ "$host_md5" == "$vm1_md5" ]; then
+        echo -n -e "MD5 match\t" | tee -a /root/test.log
 else
-        echo "MD5 doesn't match"
+        echo -n -e "MD5 doesn't match\t" | tee -a /root/test.log
 fi
-echo "Check file size from Host and Guest VMs:"
-if [ "$host" == "$vm1" ] && [ "$vm1" == "$vm2" ]; then
-        echo "File size match"
+echo "Check size from Host and Guest VM1:"
+if [ "$host" == "$vm1" ]; then
+        echo "Size match" | tee -a /root/test.log
 else
-        echo "File size doesn't match"
+        echo "Size doesn't match" | tee -a /root/test.log
 fi
 for((i=1;i<=$2;i++))
 do
 echo
-echo "Loop $i"
-ssh 192.168.20.2 "dd if=/dev/zero of=random_file bs=1024k count=$1"
+#echo "Loop $i"
+echo -n -e "$i\t" | tee -a /root/test.log
 vm1_md5=$(ssh 192.168.20.2 "md5sum random_file")
 vm1_size=$(ssh 192.168.20.2 "du -sh random_file")
 echo "Copy random_file from Guest VM1 to Guest VM2"
-vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum rand
-om_file"")
-vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh ran
-dom_file"")
+vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum random_file"")
+vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh random_file"")
 scp_start=$(date +%s)
 ssh 192.168.20.2 "sshpass -p 'root' scp random_file root@192.168.20.4:~"
 scp_end=$(date +%s)
-echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
+#echo "Elapsed time:$(($scp_end-$scp_start)) seconds" 
+echo -n -e "$(($scp_end-$scp_start))\t" | tee -a /root/test.log
 echo "Check md5 from Guest VM1 and Guest VM2:"
 if [ "$vm1_md5" == "$vm2_md5" ]; then
-        echo "MD5 match"
+        echo -n -e "MD5 match\t" | tee -a /root/test.log
 else
-        echo "MD5 doesn't match"
+        echo -n -e "MD5 doesn't match\t" | tee -a /root/test.log
 fi
-echo "Check file size from Guest VM1 and Guest VM2:"
+echo "Check size from Guest VM1 and Guest VM2:"
 if [ "$vm1_size" == "$vm2_size" ]; then
-        echo "File size match"
+        echo "Size match" | tee -a /root/test.log
 else
-        echo "File size doesn't match"
+        echo "Size doesn't match" | tee -a /root/test.log
 fi
 echo
 echo "Copy random_file from Guest VM2 to Guest VM1"
-vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum rand
-om_file"")
-vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh ran
-dom_file"")
+vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum random_file"")
+vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh random_file"")
 scp_start=$(date +%s)
 ssh 192.168.20.2 "sshpass -p 'root' scp 192.168.20.4:random_file ~"
 scp_end=$(date +%s)
-echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
+#echo "Elapsed time:$(($scp_end-$scp_start)) seconds" 
+echo -n -e "\t$(($scp_end-$scp_start))\t" | tee -a /root/test.log
 vm1_md5=$(ssh 192.168.20.2 "md5sum random_file")
 vm1_size=$(ssh 192.168.20.2 "du -sh random_file")
 echo "Check md5 from Guest VM1 and Guest VM2:"
 if [ "$vm1_md5" == "$vm2_md5" ]; then
-        echo "MD5 match"
+        echo -n -e "MD5 match\t" | tee -a /root/test.log
 else
-        echo "MD5 doesn't match"
+        echo -n -e "MD5 doesn't match\t" | tee -a /root/test.log
 fi
-echo "Check file size from Guest VM1 and Guest VM2:"
+echo "Check size from Guest VM1 and Guest VM2:"
 if [ "$vm1_size" == "$vm2_size" ]; then
-        echo "File size match"
+        echo "Size match" | tee -a /root/test.log
 else
-        echo "File size doesn't match"
+        echo "Size doesn't match" | tee -a /root/test.log
 fi
 done
+end_time=$(date +%Y%m%d%H%M%S)
 echo
 echo "Start time:"
-echo "$time"
-echo "End time:" 
-date
+echo "$start_time"
+echo "End time:"
+echo "$end_time"
 fi
+mv test.log $3-$4-$5-$start_time-$end_time.csv
+
