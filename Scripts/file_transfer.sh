28dc9529e7be1c1174fb25431059a2e88b704499
chaochin <earthshaking001@hotmail.com>
2013-04-25 14:20:11 +0800

    Update file_transfer.sh

--- Scripts/file_transfer.sh
+++ Scripts/file_transfer.sh
@@ -3,7 +3,7 @@ if [ "$1" == "" ] || [ "$2" == "" ]; then
         echo "     ###################################"
         echo "     # file_transfer [Count] [Loop]    #"
         echo "     # Example: file_transfer 2 5      #"
-        echo "     # File size:2MB, Number of loop:5 #" 
+        echo "     # File size:2MB, Number of loop:5 #"
         echo "     ###################################"
 else
 echo -e "Loop\tElapsed time(seconds)\tMD5 comparison result\tSize comparison result" | tee -a /root/test.log
@@ -20,13 +20,17 @@ echo -n -e "\t$(($scp_end-$scp_start))\t" | tee -a /root/test.log
 vm1_md5=$(ssh 192.168.20.2 "md5sum random_file")
 vm1_size=$(ssh 192.168.20.2 "du -sh random_file")
 echo "Check md5 from Host and Guest VM1:"
-if [ "$host_md5" == "$vm1_md5" ]; then
+if [ "$host_md5" == "" ] || [ "$vm1_md5" == "" ]; then
+        echo -n -e "No route to host\t" | tee -a /root/test.log
+elif [ "$host_md5" == "$vm1_md5" ]; then
         echo -n -e "MD5 match\t" | tee -a /root/test.log
 else
         echo -n -e "MD5 doesn't match\t" | tee -a /root/test.log
 fi
 echo "Check size from Host and Guest VM1:"
-if [ "$host" == "$vm1" ]; then
+if [ "$host_size" == "" ] || [ "$vm1_size" == "" ]; then
+        echo "No route to host" | tee -a /root/test.log
+elif [ "$host_size" == "$vm1_size" ]; then
         echo "Size match" | tee -a /root/test.log
 else
         echo "Size doesn't match" | tee -a /root/test.log
@@ -36,24 +40,28 @@ do
 echo
 #echo "Loop $i"
 echo -n -e "$i\t" | tee -a /root/test.log
+echo "Copy random_file from Guest VM1 to Guest VM2"
 vm1_md5=$(ssh 192.168.20.2 "md5sum random_file")
 vm1_size=$(ssh 192.168.20.2 "du -sh random_file")
-echo "Copy random_file from Guest VM1 to Guest VM2"
-vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum random_file"")
-vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh random_file"")
 scp_start=$(date +%s)
 ssh 192.168.20.2 "sshpass -p 'root' scp random_file root@192.168.20.4:~"
 scp_end=$(date +%s)
-#echo "Elapsed time:$(($scp_end-$scp_start)) seconds" 
+#echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
 echo -n -e "$(($scp_end-$scp_start))\t" | tee -a /root/test.log
+vm2_md5=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "md5sum random_file"")
+vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh random_file"")
 echo "Check md5 from Guest VM1 and Guest VM2:"
-if [ "$vm1_md5" == "$vm2_md5" ]; then
+if [ "$vm1_md5" == "" ] || [ "$vm2_md5" == "" ]; then
+        echo -n -e "No route to host\t" | tee -a /root/test.log
+elif [ "$vm1_md5" == "$vm2_md5" ]; then
         echo -n -e "MD5 match\t" | tee -a /root/test.log
 else
         echo -n -e "MD5 doesn't match\t" | tee -a /root/test.log
 fi
 echo "Check size from Guest VM1 and Guest VM2:"
-if [ "$vm1_size" == "$vm2_size" ]; then
+if [ "$vm1_size" == "" ] || [ "$vm2_size" == "" ]; then
+        echo "No route to host" | tee -a /root/test.log
+elif [ "$vm1_size" == "$vm2_size" ]; then
         echo "Size match" | tee -a /root/test.log
 else
         echo "Size doesn't match" | tee -a /root/test.log
@@ -65,18 +73,22 @@ vm2_size=$(ssh 192.168.20.2 "sshpass -p 'root' ssh root@192.168.20.4 "du -sh ran
 scp_start=$(date +%s)
 ssh 192.168.20.2 "sshpass -p 'root' scp 192.168.20.4:random_file ~"
 scp_end=$(date +%s)
-#echo "Elapsed time:$(($scp_end-$scp_start)) seconds" 
+#echo "Elapsed time:$(($scp_end-$scp_start)) seconds"
 echo -n -e "\t$(($scp_end-$scp_start))\t" | tee -a /root/test.log
 vm1_md5=$(ssh 192.168.20.2 "md5sum random_file")
 vm1_size=$(ssh 192.168.20.2 "du -sh random_file")
 echo "Check md5 from Guest VM1 and Guest VM2:"
-if [ "$vm1_md5" == "$vm2_md5" ]; then
+if [ "$vm1_md5" == "" ] || [ "$vm2_md5" == "" ]; then
+        echo -n -e "No route to host\t" | tee -a /root/test.log
+elif [ "$vm1_md5" == "$vm2_md5" ]; then
         echo -n -e "MD5 match\t" | tee -a /root/test.log
 else
         echo -n -e "MD5 doesn't match\t" | tee -a /root/test.log
 fi
 echo "Check size from Guest VM1 and Guest VM2:"
-if [ "$vm1_size" == "$vm2_size" ]; then
+if [ "$vm1_size" == "" ] || [ "$vm2_size" == "" ]; then
+        echo "No route to host" | tee -a /root/test.log
+elif [ "$vm1_size" == "$vm2_size" ]; then
         echo "Size match" | tee -a /root/test.log
 else
         echo "Size doesn't match" | tee -a /root/test.log
@@ -90,4 +102,3 @@ echo "End time:"
 echo "$end_time"
 fi
 mv test.log $3-$4-$5-$start_time-$end_time.csv
-
